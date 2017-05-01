e2e569919067a0bdcd9c46272dd2e1169a3e6078
khurana3773 <khurana3773@gmail.com>
2017-05-01 00:44:52 -0700

    cart
    cart

--- app/cart/cart.js
+++ app/cart/cart.js
@@ -15,38 +15,93 @@ var cart = angular.module('myApp.cart', ['ngRoute']);
     cart.controller('cartCtrl', ['$scope','$http',function($scope,$http) {
 
        // $scope.$apply($scope.itemss );
-        $scope.init =     function () {
+        $scope.showAllItems =     function () {
 
             $http({
                 method: 'GET',
                 url: 'http://localhost:9090/v3/starbucks/cart'
             }).then(function successCallback(response) {
-                alert(response.data.items);
+   //             alert("cart received");
 
 
-
-                $scope.itemss = [
-                    {
-                        "qty" : 3.0,
-                        "name" : "BOBA1",
-                        "milk" : "whole",
-                        "size" : "XXXlarge"
-                    },
-                    {
-                        "qty" : 3.0,
-                        "name" : "BOBA2",
-                        "milk" : "whole",
-                        "size" : "XXXlarge"
-                    }
-                ];
-
-
-
-                $scope.items = response.data.items;
+                $scope.order = response.data;
             }, function errorCallback(response) {
                 alert("Fail");
             });
         }
+
+
+        $scope.dataObj = {
+
+
+
+                    "qty": 1,
+                    "name": "latte",
+                    "milk": "whole",
+                    "size": "large"
+
+
+        };
+
+        $scope.removeItem = function(index)
+        {
+            $scope.order.items.splice(index,1);
+            var res = $http.post('http://localhost:9090/v3/starbucks/cart', $scope.order);
+            res.success(function(data, status, headers, config) {
+     //           alert( "Success message: " +status + JSON.stringify({data: data}));
+            });
+            res.error(function(data, status, headers, config) {
+                alert( "failure message: " + JSON.stringify({data: data}));
+            });
+        }
+
+        $scope.addItem = function()
+        {
+
+            var tempOrder = $scope.order;
+
+
+            $scope.order.items.push($scope.dataObj);
+
+            var res = $http.post('http://localhost:9090/v3/starbucks/cart', $scope.order);
+            res.success(function(data, status, headers, config) {
+     //           alert( "Success message: " +status + JSON.stringify({data: data}));
+                //$scope.order.items.splice(0,0,$scope.dataObj);
+
+            });
+            res.error(function(data, status, headers, config) {
+                alert( "failed to add: " + JSON.stringify({data: data}));
+            });
+
+
+
+        }
+
+
+
+
+        $scope.pay = function()
+        {
+
+            var tempOrder = $scope.order;
+
+
+
+
+            var res = $http.post('http://localhost:9090/v3/starbucks/cart', $scope.order.id);
+            res.success(function(data, status, headers, config) {
+                //           alert( "Success message: " +status + JSON.stringify({data: data}));
+                //$scope.order.items.splice(0,0,$scope.dataObj);
+
+            });
+            res.error(function(data, status, headers, config) {
+                alert( "failed to add: " + JSON.stringify({data: data}));
+            });
+
+
+
+        }
+
 
 
 
